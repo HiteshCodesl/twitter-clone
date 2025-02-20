9f5bcd7c0e925977130483334b0396e575f2bdd6
Hitesh Navare <navarehitesh65@gmail.com>
2025-02-20 14:06:47 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-300'>
+        hello there
+      </div>
+    </>
+  )
+}
+
+export default App
